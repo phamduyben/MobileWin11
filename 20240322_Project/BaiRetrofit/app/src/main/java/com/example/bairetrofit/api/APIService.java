c99d99e43b9049d36f3fa2d7a2befa42b6a789ef
Ben <duychien152003@gmail.com>
2024-03-22 10:32:50 +0700

    mssv 22162006

---
+++ 20240322_Project/BaiRetrofit/app/src/main/java/com/example/bairetrofit/api/APIService.java
@@ -0,0 +1,13 @@
+package com.example.bairetrofit.api;
+
+import com.example.bairetrofit.model.Category;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface APIService {
+    @GET("categories.php")
+    Call<List<Category>> getCategoryAll();
+}
