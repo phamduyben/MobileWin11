c67a0b59141d7f8d37a00932109779db45f761ab
Ben <duychien152003@gmail.com>
2024-04-05 15:39:13 +0700

    mssv 22162006

---
+++ 20240405_Project/Bai1/app/src/main/java/com/example/bt_29_3_2024/model/DetailActivity.java
@@ -0,0 +1,104 @@
+package com.example.bt_29_3_2024.model;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+import com.example.bt_29_3_2024.R;
+import com.example.bt_29_3_2024.api.APIService;
+import com.example.bt_29_3_2024.api.RetrofitClient;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class DetailActivity extends AppCompatActivity {
+    APIService apiService;
+    ImageView imageViewDetail;
+    TextView textViewPrice, textViewMeal, textViewArea, textViewCategory, textViewInstructions;
+    NewMeal newMeal;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        addNewMeal();
+
+        imageViewDetail = findViewById(R.id.img_detail);
+        textViewPrice = findViewById(R.id.tv_price);
+
+    }
+
+    private void addNewMeal() {
+        apiService = RetrofitClient.getRetrofit()
+                                   .create(APIService.class);
+        apiService.getNewMeals(52765)
+                  .enqueue(new Callback<ResponseBody>() {
+                      @Override
+                      public void onResponse(
+                              @NonNull Call<ResponseBody> call,
+                              @NonNull Response<ResponseBody> response
+                      ) {
+                          if (response.isSuccessful()) {
+                              try {
+                                  String res = response.body()
+                                                       .string();
+
+                                  JSONObject jsonObject =
+                                          new JSONObject(res).getJSONArray("result")
+                                                             .getJSONObject(0);
+
+                                  newMeal = new NewMeal(
+                                          jsonObject.getString("id"),
+                                          jsonObject.getString("meal"),
+                                          jsonObject.getString("area"),
+                                          jsonObject.getString("category"),
+                                          jsonObject.getString("instructions"),
+                                          jsonObject.getString("strmealthumb"),
+                                          jsonObject.getString("price")
+                                  );
+
+                                  Toast.makeText(
+                                               DetailActivity.this,
+                                               newMeal.getStrmealthumb(),
+                                               Toast.LENGTH_SHORT
+                                       )
+                                       .show();
+
+                                  Glide.with(DetailActivity.this)
+                                       .load(newMeal.getStrmealthumb())
+                                       .into(imageViewDetail);
+
+                                  textViewPrice.setText(newMeal.getPrice());
+                              } catch (IOException | JSONException e) {
+                                  e.printStackTrace();
+                              }
+                          } else {
+                              int statusCode = response.code();
+                              Log.e("Error", "onResponse: " + statusCode);
+                          }
+                      }
+
+                      @Override
+                      public void onFailure(
+                              @NonNull Call<ResponseBody> call,
+                              @NonNull Throwable t
+                      ) {
+                          Log.e("Error", "onFailure: " + t.getMessage());
+                      }
+                  });
+    }
+}
